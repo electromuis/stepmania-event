d977cd722042175f9e8895d8ea78782fb34660b5
electromuis <anton.grootes11@gmail.com>
2019-07-11 13:17:24 +0200

    Added Padmiss score fields

--- src/StatsManager.cpp
+++ src/StatsManager.cpp
@@ -318,6 +318,8 @@ void StatsManager::SavePadmissScore( const StageStats *pSS, PlayerNumber pn )
 
 	Profile *pp = PROFILEMAN->GetProfile( playerStats->m_player_number );
 	xml->AppendChild( "ScoreValue", playerStats->GetPercentDancePoints() );
+	xml->AppendChild( "Passed", !playerStats->m_bFailed );
+	xml->AppendChild( "SecondsSurvived", playerStats->m_fAliveSeconds );
 	xml->AppendChild( "PlayerNumber", playerStats->m_player_number );
 	xml->AppendChild( "PlayerName", pp->m_sDisplayName );
 	xml->AppendChild( "PlayerGuid", pp->m_sGuid );
@@ -330,6 +332,7 @@ void StatsManager::SavePadmissScore( const StageStats *pSS, PlayerNumber pn )
 	stepdata->AppendChild( "Meter", steps->GetMeter() );
 	stepdata->AppendChild( "StepArtist", steps->GetCredit() );
 	stepdata->AppendChild( "StepsType", steps->m_StepsTypeStr );
+	stepdata->AppendChild( "Group", song->m_sGroupName );
 	RageFileObjMem f;
 	vector<Steps*> stepv;
 	stepv.push_back(steps);
