a53bb0a226179720971e1438c0dff1140826a89e
Electromuis <anton.grootes11@gmail.com>
2020-11-15 21:01:17 +0100

    Draft padmiss json writer added, also ssc files supported

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,7 @@
 #include "RageFileManager.h"
 
 #include "NotesLoaderSM.h"
+#include "NotesLoaderSSC.h"
 #include "NotesWriterPadmiss.h"
 #include "Song.h"
 
@@ -14,8 +15,9 @@ using namespace std;
 
 #define SAFE_DELETE(p)       do { delete (p);     (p)=nullptr; } while( false )
 
+void convert_charts(const Song & pSong, const RString file);
+
 int main(int argc, char* argv[]) {
-	//return sm_main(argc, argv);
 
 	HOOKS = ArchHooks::Create();
 	HOOKS->Init();
@@ -26,11 +28,13 @@ int main(int argc, char* argv[]) {
 	FILEMAN = new RageFileManager(argv[0]);
 	FILEMAN->MountInitialFilesystems();
 
+	LOG = new RageLog;
+
 	RString sPath = "/";
 
 	vector<RString> files;
-	SMLoader loaderSM;
 
+	SMLoader loaderSM;
 	loaderSM.GetApplicableFiles(sPath, files);
 
 	for (auto f : files) {
@@ -44,28 +48,55 @@ int main(int argc, char* argv[]) {
 			continue;
 		}
 
-		int chartNum = 0;
-		for (auto step : pSong->GetAllSteps()) {
-			RString outputFile = f + "-" + std::to_string(chartNum) + ".json";
-
-			if (!FILEMAN->DoesFileExist(outputFile)) {
-				NotesWriterPadmiss::WriteSteps(outputFile, *step);
-
-				std::cout << "Wrote json for: " << outputFile << "\n";
-			}
-			else {
-				std::cout << outputFile << " already exists\n";
-			}
-
-			chartNum++;
-		}
-
+		convert_charts(*pSong, f);
 		SAFE_DELETE(pSong);
 	}
+
+	files.clear();
+
+	SSCLoader loaderSSC;
+	loaderSSC.GetApplicableFiles(sPath, files);
+
+	for (auto f : files) {
+		Song* pSong = new Song;
+
+		if (loaderSSC.LoadFromSimfile(f, *pSong, false)) {
+			std::cout << "Loaded: " << f << "\n";
+		}
+		else {
+			std::cout << "Failed loading: " << f << "\n";
+			continue;
+		}
+
+		convert_charts(*pSong, f);
+		SAFE_DELETE(pSong);
+	}
+
+	files.clear();
 
 	SAFE_DELETE(FILEMAN);
 	SAFE_DELETE(LUA);
 	SAFE_DELETE(PREFSMAN);
+	SAFE_DELETE(LOG);
+}
+
+void convert_charts(const Song& pSong, const RString file)
+{
+	int chartNum = 0;
+	for (auto step : pSong.GetAllSteps()) {
+		RString outputFile = file + "-" + std::to_string(chartNum) + ".json";
+
+		if (!FILEMAN->DoesFileExist(outputFile)) {
+			NotesWriterPadmiss::WriteSteps(outputFile, *step);
+
+			std::cout << "Wrote json for: " << outputFile << "\n";
+		}
+		else {
+			std::cout << outputFile << " already exists\n";
+		}
+
+		chartNum++;
+	}
 }
 
 /*
--- src/NotesWriterPadmiss.cpp
+++ src/NotesWriterPadmiss.cpp
@@ -11,44 +11,53 @@
 
 static void Serialize(const TimingSegment &seg, Json::Value &root)
 {
-	root["Beat"] = seg.GetBeat();
+	root["beat"] = seg.GetBeat();
+
 	if (seg.GetType() == SEGMENT_BPM)
 	{
-		root["BPM"] = static_cast<BPMSegment &>(const_cast<TimingSegment &>(seg)).GetBPM();
+		root["bpm"] = static_cast<BPMSegment &>(const_cast<TimingSegment &>(seg)).GetBPM();
 	}
-	else
+	else if(seg.GetType() == SEGMENT_STOP)
 	{
-		root["Seconds"] = static_cast<StopSegment &>(const_cast<TimingSegment &>(seg)).GetPause();
+		root["duration"] = static_cast<StopSegment &>(const_cast<TimingSegment &>(seg)).GetPause();
 	}
-}
-
-static void Serialize(const TimingData &td, Json::Value &root)
-{
-	JsonUtil::SerializeVectorPointers( td.GetTimingSegments(SEGMENT_BPM), Serialize, root["BpmSegments"] );
-	JsonUtil::SerializeVectorPointers( td.GetTimingSegments(SEGMENT_STOP), Serialize, root["StopSegments"] );
-}
-
-static void Serialize(const LyricSegment &o, Json::Value &root)
-{
-	root["StartTime"] = (float)o.m_fStartTime;
-	root["Lyric"] = o.m_sLyric;
-	root["Color"] = o.m_Color.ToString();
-}
-
-static void Serialize(const BackgroundDef &o, Json::Value &root)
-{
-	root["Effect"] = o.m_sEffect;
-	root["File1"] = o.m_sFile1;
-	root["File2"] = o.m_sFile2;
-	root["Color1"] = o.m_sColor1;
-}
-
-static void Serialize(const BackgroundChange &o, Json::Value &root )
-{
-	Serialize( o.m_def, root["Def"] );
-	root["StartBeat"] = o.m_fStartBeat;
-	root["Rate"] = o.m_fRate;
-	root["Transition"] = o.m_sTransition;
+	else if(seg.GetType() == SEGMENT_DELAY)
+	{
+		root["duration"] = static_cast<DelaySegment &>(const_cast<TimingSegment &>(seg)).GetPause();
+	}
+	else if (seg.GetType() == SEGMENT_WARP)
+	{
+		root["length"] = static_cast<WarpSegment&>(const_cast<TimingSegment&>(seg)).GetLength();
+	}
+	else if (seg.GetType() == SEGMENT_TIME_SIG)
+	{
+		auto segment = static_cast<TimeSignatureSegment&>(const_cast<TimingSegment&>(seg));
+		root["notesPerMeasure"] = segment.GetNoteRowsPerMeasure();
+		//todo not sure
+		root["noteValue"] = segment.GetDen();
+	}
+	else if (seg.GetType() == SEGMENT_TICKCOUNT)
+	{
+		root["ticksPerBeat"] = static_cast<TickcountSegment&>(const_cast<TimingSegment&>(seg)).GetTicks();
+	}
+	else if (seg.GetType() == SEGMENT_COMBO)
+	{
+		auto segment = static_cast<ComboSegment&>(const_cast<TimingSegment&>(seg));
+		root["comboAmount"] = segment.GetCombo();
+		root["missComboAmount"] = segment.GetMissCombo();
+	}
+	else if (seg.GetType() == SEGMENT_SCROLL)
+	{
+		root["ratio"] = static_cast<ScrollSegment&>(const_cast<TimingSegment&>(seg)).GetRatio();
+	}
+	else if (seg.GetType() == SEGMENT_FAKE)
+	{
+		root["length"] = static_cast<FakeSegment&>(const_cast<TimingSegment&>(seg)).GetLength();
+	}
+	else if (seg.GetType() == SEGMENT_LABEL)
+	{
+		root["label"] = static_cast<LabelSegment&>(const_cast<TimingSegment&>(seg)).GetLabel();
+	}
 }
 
 static void Serialize( const TapNote &o, Json::Value &root )
@@ -72,117 +81,86 @@ static void Serialize( const TapNote &o, Json::Value &root )
 		root["PlayerNumber"] = (int)o.pn;
 }
 
-static void Serialize( const NoteData &o, Json::Value &root )
+static void Serialize( const Steps& chart, Json::Value &measureArray )
 {
-	root = Json::Value(Json::arrayValue);
-	for(int t=0; t < o.GetNumTracks(); t++ )
+	RString chartString;
+	chart.GetSMNoteData(chartString);
+
+	vector<RString> measures;
+	split(chartString, ",", measures, true );
+
+	measureArray = Json::Value(Json::arrayValue);
+
+	for( auto measure : measures )
 	{
-		NoteData::TrackMap::const_iterator begin, end;
-		o.GetTapNoteRange( t, 0, MAX_NOTE_ROW, begin, end );
-		for( ; begin != end; ++begin )
+		vector<RString> noteRows;
+		split(measure, "\n", noteRows, true);
+
+		measureArray.resize(measureArray.size() + 1);
+		Json::Value& measureItem = measureArray[measureArray.size() - 1];
+		measureItem["notes"] = Json::Value(Json::arrayValue);
+
+		for (auto noteRow : noteRows)
 		{
-			int iRow = begin->first;
-			TapNote tn = begin->second;
-			root.resize( root.size()+1 );
-			Json::Value &root2 = root[ root.size()-1 ];
-			root2 = Json::Value(Json::arrayValue);
-			root2.resize(3);
-			root2[(unsigned)0] = NoteRowToBeat(iRow);
-			root2[1] = t;
-			Serialize( tn, root2[2] );
+			Trim(noteRow);
+			if (noteRow.length() == 0) {
+				continue;
+			}
+
+			measureItem["notes"].resize(measureItem["notes"].size() + 1);
+			Json::Value& rowItem = measureItem["notes"][measureItem["notes"].size() - 1];
+			rowItem = noteRow;
 		}
 	}
-}
-
-static void Serialize( const RadarValues &o, Json::Value &root )
-{
-	FOREACH_ENUM( RadarCategory, rc )
-	{
-		root[ RadarCategoryToString(rc) ] = o[rc];
-	}
-}
-
-static void Serialize( const Steps &o, Json::Value &root )
-{
-	root["StepsType"] = StringConversion::ToString(o.m_StepsType);
-
-	o.Decompress();
-
-	NoteData nd;
-	o.GetNoteData( nd );
-	Serialize( nd, root["NoteData"] );
-	root["Hash"] = o.GetHash();
-	root["Description"] = o.GetDescription();
-	root["Difficulty"] = DifficultyToString(o.GetDifficulty());
-	root["Meter"] = o.GetMeter();
-	Serialize( o.GetRadarValues( PLAYER_1 ), root["RadarValues"] );
 }
 
 bool NotesWriterPadmiss::WriteSteps( const RString &sFile, const Steps &chart )
 {
-	Song &out = *chart.m_pSong;
+	Song &song = *chart.m_pSong;
 
 	Json::Value root;
-	root["SongDir"] = out.GetSongDir();
-	root["GroupName"] = out.m_sGroupName;
-	root["Title"] = out.m_sMainTitle;
-	root["SubTitle"] = out.m_sSubTitle;
-	root["Artist"] = out.m_sArtist;
-	root["TitleTranslit"] = out.m_sMainTitleTranslit;
-	root["SubTitleTranslit"] = out.m_sSubTitleTranslit;
-	root["Genre"] = out.m_sGenre;
-	root["Credit"] = out.m_sCredit;
-	root["Banner"] = out.m_sBannerFile;
-	root["Background"] = out.m_sBackgroundFile;
-	root["LyricsFile"] = out.m_sLyricsFile;
-	root["CDTitle"] = out.m_sCDTitleFile;
-	root["Music"] = out.m_sMusicFile;
-	root["Offset"] = out.m_SongTiming.m_fBeat0OffsetInSeconds;
-	root["SampleStart"] = out.m_fMusicSampleStartSeconds;
-	root["SampleLength"] = out.m_fMusicSampleLengthSeconds;
-	if (out.m_SelectionDisplay == Song::SHOW_ALWAYS)
-		root["Selectable"] = "YES";
-	else if (out.m_SelectionDisplay == Song::SHOW_NEVER)
-		root["Selectable"] = "NO";
-	else
-		root["Selectable"] = "YES";
 
-	root["FirstBeat"] = out.GetFirstBeat();
-	root["LastBeat"] = out.GetLastBeat();
-	root["SongFileName"] = out.m_sSongFileName;
-	root["HasMusic"] = out.m_bHasMusic;
-	root["HasBanner"] = out.m_bHasBanner;
-	root["MusicLengthSeconds"] = out.m_fMusicLengthSeconds;
+	root["version"] = "1.0";
+	root["playMode"] = "unknown";
 
-	root["DisplayBpmType"] = StringConversion::ToString(out.m_DisplayBPMType);
-	if (out.m_DisplayBPMType == DISPLAY_BPM_SPECIFIED)
-	{
-		root["SpecifiedBpmMin"] = out.m_fSpecifiedBPMMin;
-		root["SpecifiedBpmMax"] = out.m_fSpecifiedBPMMax;
+	root["chartName"] = chart.GetChartName();
+	root["chartDescription"] = chart.GetDescription();
+	root["chartStyle"] = chart.GetChartStyle();
+	root["chartDifficulty"] = chart.GetDifficulty();
+	root["chartDifficultyLevel"] = chart.GetMeter();
+	root["stepArtistName"] = chart.GetCredit();
+
+	Serialize(chart, root["measures"]);
+
+	root["songPack"] = song.m_sGroupName;
+	root["songTitle"] = song.m_sMainTitle;
+	root["songSubTitle"] = song.m_sSubTitle;
+	root["songArtist"] = song.m_sArtist;
+	root["songTitleTranslation"] = song.m_sMainTitleTranslit;
+	root["songSubTitleTranslation"] = song.m_sSubTitleTranslit;
+	root["songArtistTranslation"] = song.m_sArtistTranslit;
+	root["songGenre"] = song.m_sGenre;
+
+	auto timingData = chart.GetTimingData();
+	root["musicOffset"] = timingData->m_fBeat0OffsetInSeconds;
+
+	if (song.m_fSpecifiedBPMMin == song.m_fSpecifiedBPMMax) {
+		root["displayBpm"] = std::to_string(song.m_fSpecifiedBPMMin);
+	}
+	else {
+		root["displayBpm"] = std::to_string(song.m_fSpecifiedBPMMin) + " - " + std::to_string(song.m_fSpecifiedBPMMax);
 	}
 
-	Serialize(out.m_SongTiming, root["TimingData"]);
-	JsonUtil::SerializeVectorObjects(out.m_LyricSegments, Serialize, root["LyricSegments"]);
-
-	{
-		Json::Value& root2 = root["BackgroundChanges"];
-		FOREACH_BackgroundLayer(bl)
-		{
-			Json::Value& root3 = root2[bl];
-			const vector<BackgroundChange>& vBgc = out.GetBackgroundChanges(bl);
-			JsonUtil::SerializeVectorObjects(vBgc, Serialize, root3);
-		}
-	}
-
-	{
-		const vector<BackgroundChange>& vBgc = out.GetForegroundChanges();
-		JsonUtil::SerializeVectorObjects(vBgc, Serialize, root["ForegroundChanges"]);
-	}
-
-	JsonUtil::SerializeArrayValues(out.m_vsKeysoundFile, root["KeySounds"]);
-
-
-	Serialize(chart, root["Chart"]);
+	JsonUtil::SerializeVectorPointers(timingData->GetTimingSegments(SEGMENT_BPM), Serialize, root["bpms"]);
+	JsonUtil::SerializeVectorPointers(timingData->GetTimingSegments(SEGMENT_STOP), Serialize, root["stops"]);
+	JsonUtil::SerializeVectorPointers(timingData->GetTimingSegments(SEGMENT_DELAY), Serialize, root["delays"]);
+	JsonUtil::SerializeVectorPointers(timingData->GetTimingSegments(SEGMENT_WARP), Serialize, root["warps"]);
+	JsonUtil::SerializeVectorPointers(timingData->GetTimingSegments(SEGMENT_TIME_SIG), Serialize, root["timeSignatures"]);
+	JsonUtil::SerializeVectorPointers(timingData->GetTimingSegments(SEGMENT_TICKCOUNT), Serialize, root["ticks"]);
+	JsonUtil::SerializeVectorPointers(timingData->GetTimingSegments(SEGMENT_COMBO), Serialize, root["combos"]);
+	JsonUtil::SerializeVectorPointers(timingData->GetTimingSegments(SEGMENT_COMBO), Serialize, root["scrolls"]);
+	JsonUtil::SerializeVectorPointers(timingData->GetTimingSegments(SEGMENT_COMBO), Serialize, root["fakes"]);
+	JsonUtil::SerializeVectorPointers(timingData->GetTimingSegments(SEGMENT_COMBO), Serialize, root["labels"]);
 	
 	return JsonUtil::WriteFile(root, sFile, false);
 }
